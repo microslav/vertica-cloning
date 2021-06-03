69771b24fdc45977b35721da18f9fbc0dcd16506
microslav <miroslavk@upenn.alumni.edu>
2021-06-02 17:26:22 -0700

    Add explanatory comments to the env settings file

--- source_me.sh
+++ source_me.sh
@@ -13,37 +13,50 @@ fi
 
 set -x
 
-### Common info
+### Common info #######################################################
+# Vertica database admin user
 export RUN_AS="dbadmin"
+# Maximum replication lag between FlashBlades to wait before trying revive
 export LAG_MAX_WAIT="900"
+# Whether to flush internal stats and delete old data prior to replication (rarely useful)
 export FLUSH_SOURCE="no"
+# Table in default schema to use for simple count(*) test
 export CANARY_TABLE="WebPages"
 
 ### Source-side info for the DR demo
+# Source (and cloning) FlashBlade Management IP address
 export SRC_FB_MGMT="10.99.100.100"
+# Source (and cloning) FlashBlade Data IP address
 export SRC_FB_DATA="10.99.100.101"
+# Source (and cloning) FlashBlade API Token
 export SRC_FB_API="T-deadbeef-f00d-cafe-feed-1337c0ffee"
+# Bucket on the Source (and clone) side used for communal storage
 export SRC_BUCKET="bos"
+# Object key prefix within the bucket where the source database lives
 export SRC_PREFIX="/prod/"
+# AWS config and credentials profile to use on the source side
 export SRC_AWS_PROFILE="default"
+# Alias for s5cmd used on the source side
 export SRC_S5="AWS_PROFILE=${SRC_AWS_PROFILE} s5cmd --endpoint-url=http://${SRC_FB_DATA}:80"
 
 ### Destination/Target-side info for the DR demo
+# Destination FlashBlade Management IP address
 export DST_FB_MGMT="10.99.101.100"
+# Destination FlashBlade Data IP address
 export DST_FB_DATA="10.99.101.101"
+# Destination FlashBlade API Token
 export DST_FB_API="T-deadbeef-f00d-cafe-feed-1337c0ffee"
+# Bucket on the destination side used as the replication target
 export DST_BUCKET="bos-dr"
+# Object key prefix to substitute for the source prefix in the cloned path
 export DST_PREFIX="/test/"
+# AWS config and credentials profile to use on the destination side
 export DST_AWS_PROFILE="default"
+# Alias for s5cmd used on the destination side
 export DST_S5="AWS_PROFILE=${DST_AWS_PROFILE} s5cmd --endpoint-url=http://${DST_FB_DATA}:80"
+# Path to auth_params.conf file used on the destination cluster for DR testing (DR target FlashBlade endpoint)
 export DST_DR_AUTH="/home/${RUN_AS}/dr_auth_params.conf"
+# Path to auth_params.conf file used on the destination cluster for Cloning testing (source FlashBlade endpoint)
 export DST_CLONE_AUTH="/home/${RUN_AS}/clone_auth_params.conf"
-
-### Info for the Cloning demo
-export PUREFB_API="$DST_FB_API"
-export PUREFB_URL="$DST_FB_MGMT"
-export PUREFB_DAT="$DST_FB_DATA"
-export CLN_SRC_PRE="$SRC_PREFIX"
-export CLN_DST_PRE="$DST_PREFIX"
 
 set +x
